e942be90e494ea53fd7ecfcc9a56dbea988301a2
mengxy23 <mengxy23@jlu.edu.cn>
2025-04-24 09:19:33 +0800

    Delete hot.js

--- hot.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const r = await fetch("https://apis.tianapi.com/hot/index?key=c652bd1f2d34516b23d1e55e9cd98634");
-  const data = await r.json();
-  res.status(200).json(data);
-}
